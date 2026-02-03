e8b7925cd995e864e1db1e7866b07420722a1eff
Meathill <meathill@gmail.com>
2026-02-03 14:08:21 +0800

    bug: fixed deployment

---
+++ src/types/remote-modules.d.ts
@@ -0,0 +1,13 @@
+declare module 'https://unpkg.com/@roudanio/awesome-auth@0.1.5/dist/awesome-auth.js' {
+  export function getInstance(options: { googleId: string; root: string }): unknown;
+}
+
+declare module 'https://unpkg.com/@roudanio/awesome-comment@0.10.3/dist/awesome-comment.js' {
+  const AwesomeComment: {
+    init: (
+      root: HTMLElement,
+      options: { turnstileSiteKey?: string; postId: string; apiUrl: string; awesomeAuth: unknown },
+    ) => void;
+  };
+  export default AwesomeComment;
+}
